00add5b2a0ce039f33518d8fcc4b0a9fd8487bbf
MakerSquare Student <students@themakersquare.com>
2013-10-10 10:56:44 -0500

    steps 1-11 add

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Movie do
+    let(:movie) { Movie.new }
+
+    describe "#snippet" do
+      context "when description is less than 50 characters" do
+        it "returns the full description" do
+          #set scenario
+          allow(movie).to receive(:description).and_return("When a Roman general is betrayed and his family is") 
+
+          #declare exepectation
+          expect(movie.snippet).to eq(movie.description)
+        end
+      end
+      context "when description is greater than 50 characters" do
+        it "returns the first 47 characters and appends ..."
+
+      end
+    end
+  end  
 end
