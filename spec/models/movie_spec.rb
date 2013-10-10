fa41f5d077621de26b467271f92ff1fcdd0d6486
MakerSquare Student <students@themakersquare.com>
2013-10-10 11:13:37 -0500

    failing tests for audience rating nil scenario

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Movie do
   describe Movie do
     let(:movie) { Movie.new }
+    let(:rotten_finder_return) { OpenStruct.new(ratings: OpenStruct.new(audience_score: 84))}
 
     describe "#snippet" do
       context "when description is less than 50 characters" do
@@ -15,9 +16,35 @@ describe Movie do
         end
       end
       context "when description is greater than 50 characters" do
-        it "returns the first 47 characters and appends ..."
+        it "returns the first 47 characters and appends ..." do
+          allow(movie).to receive(:description).and_return("When a Roman general is betrayed and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge.")
 
+          expect(movie.snippet).to eq("When a Roman general is betrayed and his family...")
+        end
       end
-    end
+      context "when description is nil" do
+        it "returns an empty string" do
+          allow(movie.snippet).to receive(:description).and_return(nil)
+
+          expect(movie.snippet).to eq('')
+        end
+      end 
+    end 
+    describe "#audience_rating" do
+      context "when Movie is found on Rotten Tomatoes" do
+        it "returns the audience score for the Movie" do
+          allow(movie).to receive(:rotten_finder).and_return(rotten_finder_return)
+
+          expect(movie.audience_rating).to eq(84)
+        end
+      end
+      context "when movie not found" do
+        it "returns audience rating as nil" do
+          allow(movie).to receive(:rotten_finder).and_return(nil)
+
+          expect(movie.audience_rating).to eq(nil)
+        end
+      end
+    end 
   end  
 end
